3cd6fc55858c43864b90aeb181d88db1f9803678
luiavag <luiavag@gmail.com>
2020-08-26 02:37:36 -0500

    merging ver2, with categories auto load implementation

--- version.js
+++ version.js
@@ -1,19 +1,13 @@
-const apiInfo = async function(param) {
+const apiInfo = (async function (param) {
     try {
         const response = await fetch('https://sv443.net/jokeapi/v2/info')
         const data = await response.json();
         const { version, jokes } = data;
 
-        if (param === 'version') {
-            return document.querySelector('#version').textContent = version;
-        }
-
-        if (param === 'numJokes') {
-            return document.querySelector('#numJokes').textContent = jokes.totalCount;
-        }
+        document.querySelector('#version').textContent = version
+        document.querySelector('#numJokes').textContent = jokes.totalCount
+        
     } catch (err) {
         console.log('Oooops', err);
     }
-}
-apiInfo('version');
-apiInfo('numJokes');
+})()
